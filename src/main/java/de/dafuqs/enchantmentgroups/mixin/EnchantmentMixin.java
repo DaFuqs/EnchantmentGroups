3137e6138a1585a6c8a7cf9c5eca52091f5a50e1
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-08-11 14:06:21 +0200

    failsafe for mods that tinker with enchantments at mod load time

--- src/main/java/de/dafuqs/enchantmentgroups/mixin/EnchantmentMixin.java
+++ src/main/java/de/dafuqs/enchantmentgroups/mixin/EnchantmentMixin.java
@@ -12,7 +12,7 @@ public class EnchantmentMixin {
 
     @Inject(method = "canAccept(Lnet/minecraft/enchantment/Enchantment;)Z", at = @At("RETURN"), cancellable = true)
     public final void enchantmentGroups$applyEnchantmentGroups(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
-        if(cir.getReturnValue()) {
+        if(cir.getReturnValue() && EnchantmentGroups.config != null) {
             Enchantment thisEnchantment = (Enchantment) (Object) this;
             boolean canCombine = EnchantmentGroups.config.canCombine(thisEnchantment, other);
             if(!canCombine) {
