9d95aeea9e1f63db651e136e23541412925c8f80
Vex <assassin4byt@gmail.com>
2022-10-09 18:55:37 +0200

    Improved config + treasure group

--- src/main/java/de/dafuqs/enchantmentgroups/Config.java
+++ src/main/java/de/dafuqs/enchantmentgroups/Config.java
@@ -1,5 +1,9 @@
 package de.dafuqs.enchantmentgroups;
 
+import me.wawwior.config.ConfigProvider;
+import me.wawwior.config.Configurable;
+import me.wawwior.config.IConfig;
+import me.wawwior.config.io.impl.FileInfo;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -9,91 +13,41 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Config {
+public class Config extends Configurable<Config.EnchantmentConfig, FileInfo> {
 
-    public static final String DEFAULT_CONFIG = """
-{
-    /* This default config serves as an example on how to set up groups
-       Since these enchantments are mutually exclusive by default already
-       you will probably not notice any changes when enchanting.
-       Create your own enchantment groups here or expand on these defaults, if you wish
-    */
-    "protection": [
-        "minecraft:protection",
-        "minecraft:blast_protection",
-        "minecraft:fire_protection",
-        "minecraft:projectile_protection"
-    ],
-    "melee_damage": [
-        "minecraft:bane_of_arthropods",
-        "minecraft:smite",
-        "minecraft:sharpness"
-    ],
-    "mining": [
-        "minecraft:fortune",
-        "minecraft:silk_touch"
-    ],
-    "bow": [
-        "minecraft:infinity",
-        "minecraft:mending"
-    ],
-    "trident1": [
-        "minecraft:loyalty",
-        "minecraft:riptide"
-    ],
-    "trident2": [
-        "minecraft:channeling",
-        "minecraft:riptide"
-    ],
-    "crossbow": [
-        "minecraft:multishot",
-        "minecraft:piercing"
-    ]
-}
-""";
+    public static class EnchantmentConfig implements IConfig {
+        HashMap<Identifier, List<Identifier>> exclusivityGroups = new HashMap<>();
+        List<Identifier> treasures = new ArrayList<>();
 
-    private final HashMap<Identifier, List<Identifier>> mutualExclusivities;
+    }
 
-    public Config(HashMap<String, List<String>> input) {
-        HashMap<Identifier, List<Identifier>> mutualExclusivities = new HashMap<>();
+    HashMap<Identifier, List<Identifier>> exclusivityGroups = new HashMap<>();
 
-        for(List<String> entries : input.values()) {
-            // Try parsing all entries in each list as identifiers
-            List<Identifier> entryIdentifiers = new ArrayList<>();
-            for(String entry :entries) {
-                Identifier identifier = Identifier.tryParse(entry);
-                if(identifier == null) {
-                    EnchantmentGroups.log(Level.WARN, "Config entry '" + entry + "' could not be parsed as a valid identifier. Will be ignored.");
-                } else {
-                    entryIdentifiers.add(identifier);
-                }
+    public Config(ConfigProvider<FileInfo> provider) {
+        super(EnchantmentConfig.class, FileInfo.of("", EnchantmentGroups.CONFIG_NAME), provider);
+    }
+
+    public void transform() {
+        config.exclusivityGroups.values().forEach(s -> s.forEach(i -> {
+            if(Registry.ENCHANTMENT.containsId(i)) {
+                exclusivityGroups.put(i, s);
             }
-
-            // creating a map for each identifier for fast lookup
-            for(int i = 0; i < entryIdentifiers.size(); i++) {
-                List<Identifier> currentExclusives = new ArrayList<>();
-                for (int j = 0; j < entryIdentifiers.size(); j++) {
-                    if(i != j) { // an enchantment is always incompatible with itself anyways
-                        currentExclusives.add(entryIdentifiers.get(j));
-                    }
-                }
-                mutualExclusivities.put(entryIdentifiers.get(i), currentExclusives);
-            }
-        }
-
-        this.mutualExclusivities = mutualExclusivities;
+        }));
     }
 
     public boolean canCombine(Enchantment enchantment1, Enchantment enchantment2) {
-        // we only have to check for a single enchantment here
-        // since both exclusivity lists would return either true or false everytime
         Identifier id1 = Registry.ENCHANTMENT.getId(enchantment1);
-        if(mutualExclusivities.containsKey(id1)) {
-            List<Identifier> id1Exclusivities = mutualExclusivities.get(id1);
+        if (exclusivityGroups.containsKey(id1)) {
+            List<Identifier> id1Exclusives = exclusivityGroups.get(id1);
             Identifier id2 = Registry.ENCHANTMENT.getId(enchantment2);
-            return !id1Exclusivities.contains(id2);
+            return !id1Exclusives.contains(id2);
+
         }
         return true;
+    }
+
+    public boolean isTreasure(Enchantment enchantment) {
+        return config.treasures.contains(Registry.ENCHANTMENT.getId(enchantment));
     }
 
 }
--- src/main/java/de/dafuqs/enchantmentgroups/EnchantmentGroups.java
+++ src/main/java/de/dafuqs/enchantmentgroups/EnchantmentGroups.java
@@ -1,61 +1,37 @@
 package de.dafuqs.enchantmentgroups;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
+import me.wawwior.config.ConfigProvider;
+import me.wawwior.config.io.impl.FileInfo;
+import me.wawwior.config.io.impl.JsonFileAdapter;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class EnchantmentGroups implements ModInitializer {
 
-    public static final String MODID = "enchantment_groups";
-    private static final Logger LOGGER = LogManager.getLogger();
-    public static final String CONFIG_NAME = "EnchantmentGroups.json5";
-    public static final Gson GSON = new Gson();
+    public static final String MOD_ID = "enchantment_groups";
 
-    public static Config config;
+    public static final Logger LOGGER = LoggerFactory.getLogger("EnchantmentGroups");
+    public static final String CONFIG_NAME = "EnchantmentGroups";
+
+    private static final ConfigProvider<FileInfo> provider = new ConfigProvider<>(
+            new JsonFileAdapter("config")
+                    .withAdapter(Identifier.class, new Identifier.Serializer()),
+            true
+    );
+
+    public static Config config = new Config(provider);
 
 
     @Override
     public void onInitialize() {
-        loadConfig();
-    }
-
-    public static void log(Level level, String text) {
-        LOGGER.log(level, "[EnchantmentGroups] " + text);
-    }
-
-    public void loadConfig() {
-        try {
-            File file = FabricLoader.getInstance().getConfigDir().resolve(CONFIG_NAME).toFile();
-            if (!file.exists()) {
-                EnchantmentGroups.log(Level.WARN, "'EnchantmentGroups.json5' config file not found! Creating default config.");
-                file.createNewFile();
-                FileOutputStream out = new FileOutputStream(file, false);
-                out.write(Config.DEFAULT_CONFIG.getBytes());
-                out.flush();
-                out.close();
-            }
-
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                Type type = new TypeToken<HashMap<String, List<String>>>(){}.getType();
-                config = new Config(GSON.fromJson(br, type));
-
-                EnchantmentGroups.log(Level.INFO, "Init complete");
-            } catch (Exception e) {
-                log(Level.ERROR, "Error reading config file 'EnchantmentGroups.json5': " + e.getMessage());
-            }
-        } catch (IOException e) {
-            log(Level.ERROR, "Error loading config file 'EnchantmentGroups.json5': " + e.getMessage());
-        }
+        config.load();
+        config.transform();
+        config.save();
     }
 
 }
