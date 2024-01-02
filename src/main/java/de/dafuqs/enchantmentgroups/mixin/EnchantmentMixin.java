2258b32ee8c2b4102364afab6ce3d8fcc37e1785
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-01-02 19:02:28 +0100

    update dependencies

--- src/main/java/de/dafuqs/enchantmentgroups/mixin/EnchantmentMixin.java
+++ src/main/java/de/dafuqs/enchantmentgroups/mixin/EnchantmentMixin.java
@@ -2,6 +2,7 @@ package de.dafuqs.enchantmentgroups.mixin;
 
 import de.dafuqs.enchantmentgroups.EnchantmentGroups;
 import net.minecraft.enchantment.Enchantment;
+import org.apache.logging.log4j.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +13,13 @@ public class EnchantmentMixin {
 
     @Inject(method = "canAccept(Lnet/minecraft/enchantment/Enchantment;)Z", at = @At("RETURN"), cancellable = true)
     public final void enchantmentGroups$applyEnchantmentGroups(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
+        EnchantmentGroups.log(Level.INFO, "mixin called");
         if(cir.getReturnValue() && EnchantmentGroups.config != null) {
+            EnchantmentGroups.log(Level.INFO, "mixin in if");
             Enchantment thisEnchantment = (Enchantment) (Object) this;
             boolean canCombine = EnchantmentGroups.config.canCombine(thisEnchantment, other);
             if(!canCombine) {
+                EnchantmentGroups.log(Level.INFO, "config: not combinable");
                 cir.setReturnValue(false);
             }
         }
